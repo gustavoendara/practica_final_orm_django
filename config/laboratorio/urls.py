c5277e95706f8de41afd3b68a822a1bb98577a94
gustavoendara <gustavo.mito15@gmail.com>
2024-11-12 17:45:44 -0300

    subiendo practica final orm django

--- config/laboratorio/urls.py
+++ config/laboratorio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LaboratorioListView, LaboratorioDetailView, LaboratorioCreateView, LaboratorioUpdateView, LaboratorioDeleteView, HomePageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'), #pagina de inicio
     path('lista/', LaboratorioListView.as_view(), name='laboratorio_list'),  # Listado de laboratorios
     path('detail/<int:pk>/', LaboratorioDetailView.as_view(), name='laboratorio_detail'),  # Detalles de un laboratorio
     path('create/', LaboratorioCreateView.as_view(), name='laboratorio_create'),  # Crear laboratorio
